3d646202dccde22e457ab5c03547092be80bba00
john pfeiffer <foupfeiffer@gmail.com>
2019-01-05 09:23:03 -0800

    correctly close HTML tags and Chapter header, convert newlines to html breaks, refactor tests to use subtests and a manual dev output test

--- paginator/paginator.go
+++ paginator/paginator.go
@@ -6,28 +6,31 @@ import (
 )
 
 // TableOfContentsAnchor is a string that holds the default HTML tag for the table of contents link
-const TableOfContentsAnchor = `<h3 id="TOC">`
+const TableOfContentsAnchor = `<h3 id="TOC">TOC</h3>`
 
 // Book has many chapters
 type Book struct {
 	Chapters []string `json:"chapters"`
 }
 
-// generateChapters
-func generateChapters(text string) (Book, error) {
+// generateHTMLChapters
+func generateHTMLChapters(text string) (Book, error) {
 	b := Book{Chapters: []string{}}
 	// TODO: proper parser to break into chunks
 	chapterSplit := "CHAPTER "
 	parts := strings.Split(text, chapterSplit)
-	for i, p := range parts {
-		var current string
+	for i, original := range parts {
+		var chapterHeader string
 		if i == 0 {
-			current = TableOfContentsAnchor
+			chapterHeader = TableOfContentsAnchor
 			// TODO: full TOC links outside?
 		} else {
-			current = `<h3 id="` + chapterSplit + strconv.Itoa(i) + `">` + chapterSplit
+			chapterNumber := strconv.Itoa(i)
+			// TODO: use original chapter numbers
+			chapterHeader = `<h3 id="` + strings.TrimSpace(chapterSplit) + chapterNumber + `">` + chapterSplit + chapterNumber + `</h3>`
 		}
-		b.Chapters = append(b.Chapters, current+p)
+		converted := strings.Replace(original, "\\n", "<br />", -1)
+		b.Chapters = append(b.Chapters, chapterHeader+converted)
 	}
 	return b, nil
 }
--- paginator/paginator_test.go
+++ paginator/paginator_test.go
@@ -3,34 +3,45 @@ package paginator
 import (
 	"fmt"
 	"reflect"
+	"strconv"
+	"strings"
 	"testing"
 )
 
-func TestGenerateChaptersEmpty(t *testing.T) {
-	t.Parallel()
-	result, err := generateChapters("")
-	assertErrIsNil(t, err)
-	assertStringSlicesEqual(t, []string{TableOfContentsAnchor}, result.Chapters)
-}
-
-func TestGenerateChapters(t *testing.T) {
-	t.Parallel()
-	result, err := generateChapters("CHAPTER 1")
-	assertErrIsNil(t, err)
-	assertStringSlicesEqual(t,
-		[]string{TableOfContentsAnchor, `<h3 id="CHAPTER 1">CHAPTER 1`}, result.Chapters)
+func TestManual(t *testing.T) {
+	input := ""
+	for i := 1; i < 12; i++ {
+		input += `CHAPTER ` + strconv.Itoa(i) + `\nfoobar\n\n`
+	}
+	b, _ := generateHTMLChapters(input)
+	s := `<html>` + strings.Join(b.Chapters, "<hr>\n") + `</html>`
+	fmt.Println(s)
 }
 
 func TestChapters(t *testing.T) {
 	var testCases = []struct {
+		testName string
 		s        string
 		expected []string
 	}{
-		{s: "", expected: []string{TableOfContentsAnchor}},
+		{testName: "empty", s: "",
+			expected: []string{TableOfContentsAnchor}},
+		// TODO: the extra 1 is wrong
+		{testName: "chapter only", s: "CHAPTER 1",
+			expected: []string{TableOfContentsAnchor, `<h3 id="CHAPTER1">CHAPTER 1</h3>1`}},
+		// TODO: the extra 1 is wrong
+		{testName: "single chapter", s: `CHAPTER 1\nfoobar`,
+			expected: []string{TableOfContentsAnchor, `<h3 id="CHAPTER1">CHAPTER 1</h3>1<br />foobar`}},
+		// TODO: the extra 1 and 2 are wrong
+		{testName: "two chapters", s: `CHAPTER 1\nfoobar\n\nCHAPTER 2`,
+			expected: []string{
+				TableOfContentsAnchor,
+				`<h3 id="CHAPTER1">CHAPTER 1</h3>1<br />foobar<br /><br />`,
+				`<h3 id="CHAPTER2">CHAPTER 2</h3>2`}},
 	}
 	for _, tc := range testCases {
-		t.Run(fmt.Sprintf("%s to chapters with anchors", tc.s), func(t *testing.T) {
-			result, err := generateChapters(tc.s)
+		t.Run(tc.testName, func(t *testing.T) {
+			result, err := generateHTMLChapters(tc.s)
 			assertErrIsNil(t, err)
 			assertStringSlicesEqual(t, tc.expected, result.Chapters)
 		})
